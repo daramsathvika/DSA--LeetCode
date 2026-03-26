5039cb61cf521879420a43cb8f6adf943d11aa74
Daram Sathvika <daramsathvika23@gmail.com>
2026-03-26 16:30:09 +0530

    Time: 1 ms (99.93%), Space: 80.7 MB (7.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,22 +4,9 @@ class Solution {
         boolean pos=false;
         for(i=0;i<n;i++){
             sum+=nums[i];
-            if(nums[i]>0){pos=true;}
+            if(max_sum<sum-min){max_sum=sum-min;}
+            if(sum<min){min=sum;}
         }
-        //if(pos==true){
-            sum=0;
-            for(i=0;i<n;i++){
-                sum+=nums[i];
-                if(max_sum<sum-min){max_sum=sum-min;}
-                if(sum<min){min=sum;}
-            }
-        /*}
-        else{sum=0;
-            for(i=0;i<n;i++){
-                sum+=nums[i];
-                if(max_sum<sum-min){max_sum=sum-min;}
-                if(sum<min){min=sum;}}
-            }*/
         return max_sum;
     }
 }
