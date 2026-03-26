3d2104a77a35086dddf533df55683ab88b260ef0
Daram Sathvika <daramsathvika23@gmail.com>
2026-03-26 16:30:17 +0530

    Time: 1 ms (99.93%), Space: 77.1 MB (77.5%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,7 +5,8 @@ class Solution {
         for(i=0;i<n;i++){
             sum+=nums[i];
             if(max_sum<sum-min){max_sum=sum-min;}
-            if(sum<min){min=sum;}
+            if(sum<min){min=sum;}//only either this statement or the above statement is true at a time so no need to wonder about writing else and  if we flip the order of placing these two conditions for negative numbers(array with no positive numbers) min gets update everytime and sum=min(as there are no positive numbers sum always goes incresing but in -ve) do sum-min =0 so max_sum always becomes 0 so we dont get our real answer
+            
         }
         return max_sum;
     }
