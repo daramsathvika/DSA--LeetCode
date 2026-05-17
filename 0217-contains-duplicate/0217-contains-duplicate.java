bf822495f56d7804c6a1efea2b1314aabdc11cdb
Daram Sathvika <daramsathvika23@gmail.com>
2026-05-17 11:37:18 +0530

    Time: 14 ms (88.77%), Space: 98 MB (6.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,8 +3,8 @@ class Solution {
         int i=0,j=i,n=nums.length;
         HashSet<Integer> set = new HashSet<>();
         for(i=0;i<n;i++){
-            if(!set.contains(nums[i])){set.add(nums[i]);}
-            else{return true;}
+            set.add(nums[i]);
+            if(set.size()<i+1){return true;}
         }
         return false;
     }
