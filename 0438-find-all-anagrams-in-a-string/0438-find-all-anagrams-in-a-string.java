3c6725208bbb9530ddfaa11f2a3b6870b4d64d28
Daram Sathvika <daramsathvika23@gmail.com>
2026-05-18 22:19:29 +0530

    Time: 9 ms (91.94%), Space: 46.9 MB (57.2%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,14 +2,19 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
         int arr1[] = new int[26];
+        int arr2[] = new int[26];
         int i,j,m=p.length(),n=s.length();
+        if(m>n){return list;}
         for(i=0;i<m;i++){
             arr1[p.charAt(i)-'a']++;
         }
-        for(i=0;i<n-m+1;i++){
-            int arr2[] = new int[26]; 
-            for(j=i;j<i+m;j++){arr2[s.charAt(j)-'a']++;}
-            if(Arrays.equals(arr1,arr2)){list.add(i);}
+        for(i=0;i<m;i++){
+            arr2[s.charAt(i)-'a']++;
+            if(Arrays.equals(arr1,arr2)){list.add(0);}
+        }
+        for(i=0;i<n-m;i++){
+            arr2[s.charAt(i)-'a']--;arr2[s.charAt(i+m)-'a']++;
+            if(Arrays.equals(arr1,arr2)){list.add(i+1);}
         }
         return list;
     }
