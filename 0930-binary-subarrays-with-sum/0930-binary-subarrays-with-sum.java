f53914520773ce680caddc40fc8fd9d86d1d710d
Daram Sathvika <daramsathvika23@gmail.com>
2026-05-20 17:42:56 +0530

    Time: 23 ms (33.61%), Space: 52.8 MB (8.72%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -5,7 +5,7 @@ class Solution {
         map.put(0,1);
         for(i=0;i<n;i++){
             sum+=nums[i];
-            if(sum>=goal){count+=map.getOrDefault(sum-goal,0);}
+            if(map.containsKey(sum-goal)){count+=map.getOrDefault(sum-goal,0);}
             map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return count;
