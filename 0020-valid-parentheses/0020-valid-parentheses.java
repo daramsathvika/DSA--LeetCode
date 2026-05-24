d76115db85582a49e4e5071f6eca54904771d4b4
Daram Sathvika <daramsathvika23@gmail.com>
2026-05-24 21:03:12 +0530

    Time: 5 ms (14.81%), Space: 44.1 MB (5.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int i=0,n=s.length();
+        for(i=0;i<n;i++){
+            if(s.charAt(i)=='('){st.push('(');}
+            else if(s.charAt(i)=='{'){st.push('{');}
+            else if(s.charAt(i)=='['){st.push('[');}
+            if(s.charAt(i)==')'){
+                if(!st.isEmpty()&&st.peek()=='('){st.pop();} 
+                else{System.out.print(s.charAt(i));return false;}
+            }          
+            else if(s.charAt(i)=='}'){
+                if(!st.isEmpty()&&st.peek()=='{'){st.pop();} 
+                else{System.out.print(s.charAt(i));return false;}
+            }          
+            else if(s.charAt(i)==']'){
+                if(!st.isEmpty()&&st.peek()=='['){st.pop();} 
+                else{System.out.print(s.charAt(i));return false;}
+            }          
+        }
+        if(st.size()==0){return true;}
+        return false;
+    }
+}
