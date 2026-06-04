c8a08b2363b5b92e9881f89e928acc6b978b8e64
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-04 22:50:16 +0530

    Time: 4 ms (75.54%), Space: 45.2 MB (49.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,11 +3,10 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         int i,n=nums2.length,m=nums1.length;
         HashMap<Integer,Integer> map = new HashMap<>();
-        st.push(nums2[0]);
-        for(i=0;i<n;i++){
-            while(st.size()>0&&st.peek()<nums2[i]){
-                map.put(st.pop(),nums2[i]);
-            }
+        st.push(nums2[n-1]);map.put(nums2[n-1],-1);
+        for(i=n-2;i>=0;i--){
+            while(st.size()>0&&nums2[i]>=st.peek()){st.pop();}
+            if(st.size()>0){map.put(nums2[i],st.peek());}
             st.push(nums2[i]);
         }
         int []op = new int[m];
