6f7b2cb748eb83639a616a952579a9fe3666c219
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-06 13:37:59 +0530

    Time: 1 ms (88.74%), Space: 44.3 MB (19.73%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,16 @@
 class Solution {
-    public static List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> set = new ArrayList<List<Integer>>();
-        List<Integer> list = new ArrayList<Integer>();
-        sets(list,0,nums,set);
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> set = new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
+        subset(set,list,0,nums);
         return set;
     }
-    public static void sets(List<Integer> list,int i,int arr[],List<List<Integer>> set){
-        if(i>=arr.length){
-        List<Integer> listtt = new ArrayList<Integer>(list);set.add(listtt);}
-        else{
-            list.add(arr[i]);
-            sets(list,i+1,arr,set);
-            list.remove(Integer.valueOf(arr[i]));
-            sets(list,i+1,arr,set);
-        }
+    public void subset(List<List<Integer>> set,List<Integer> list,int i,int []nums){
+        List<Integer> newlist = new ArrayList<>(list);
+        if(i==nums.length){set.add(newlist);return;}
+        newlist.add(Integer.valueOf(nums[i]));
+        subset(set,newlist,i+1,nums);
+        newlist.remove(Integer.valueOf(nums[i]));
+        subset(set,newlist,i+1,nums);
     }
 }
