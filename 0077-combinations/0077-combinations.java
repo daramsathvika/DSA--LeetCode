5c5644ea06567d1fc533fb34db13faf2fced451b
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-08 11:13:57 +0530

    Time: 18 ms (81.28%), Space: 98.1 MB (27.36%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,16 +2,16 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> set = new ArrayList<>();
         List<Integer> list= new ArrayList<>(k);
-        combi(set,list,0,k,n);
+        //for(int i=0;i<n-k+1;i++){
+        combi(set,list,1,k,n);//}
         return set;
     }
     public void combi(List<List<Integer>> set,List<Integer> list,int i,int k,int n){
-        if(list.size()==k){set.add(new ArrayList(list));}
-        //if(list.size()==k&&)
-        for(int j=i+1;j<=n;j++){
-            if(list.contains(j)||(list.size()>0&&list.get(list.size()-1)>j)){continue;}
+        if(list.size()==k){set.add(new ArrayList(list));return;}
+        for(int j=i;j<=n;j++){
+            //if(list.contains(j)||(list.size()>0&&list.get(list.size()-1)>j)){continue;}
             list.add(j);
-            combi(set,list,i+1,k,n);
+            combi(set,list,j+1,k,n);
             list.remove(list.size()-1);
         }
     }
