2a57b0599140a2d1cf019d4455b4383ecf0c23e3
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-08 20:34:04 +0530

    Time: 3 ms (55.6%), Space: 45 MB (59.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,16 +2,19 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> set = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        subsetDup(set,list,0,nums);
+        Arrays.sort(nums);
+        subDup(nums,0,set,list);
         return set;
     }
-    public void subsetDup(List<List<Integer>> set,List<Integer> list,int i,int[] nums){
-        List<Integer> newlist = new ArrayList<>(list);
-        if(i==nums.length){if(!set.contains(newlist)){set.add(newlist);} return ;}
-        newlist.add(nums[i]);
-        subsetDup(set,newlist,i+1,nums);
-        if(!list.equals(newlist)){
-        newlist.remove(Integer.valueOf(nums[i]));
-        subsetDup(set,newlist,i+1,nums); }
+    public void subDup(int []nums,int i,List<List<Integer>> set,List<Integer> list){
+        set.add(new ArrayList(list));
+        for(int j=i;j<nums.length;j++){
+            if(j>i&&nums[j]==nums[j-1]){continue;}
+            list.add(nums[j]);
+            subDup(nums,j+1,set,list);
+            list.remove(list.size()-1);
+            //subDup(nums,j+1,set,list);
+        }
     }
 }
+// 1
