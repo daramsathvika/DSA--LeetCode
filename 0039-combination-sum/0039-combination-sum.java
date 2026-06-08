aa635836601db0d2a4444185dc22629c5e1a212d
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-08 12:21:24 +0530

    Time: 9 ms (4.58%), Space: 45.6 MB (81.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -9,10 +9,11 @@ class Solution {
     public void combisum(int []cand,int target,List<List<Integer>> set,List<Integer> list,int i,int sum){
         if(sum==target){set.add(new ArrayList(list));return;}
         for(int j=i;j<cand.length;j++){
-            if(sum+cand[j]>target){return;}
-            list.add(cand[j]);
-            combisum(cand,target,set,list,j,sum+cand[j]);
-            list.remove(Integer.valueOf(cand[j]));
+            if(sum<target){
+                list.add(cand[j]);
+                combisum(cand,target,set,list,j,sum+cand[j]);
+                list.remove(Integer.valueOf(cand[j]));
+            }
         }
     }
 }
