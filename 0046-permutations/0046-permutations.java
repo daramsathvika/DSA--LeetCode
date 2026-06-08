b6e9ae6407967eb09d37db26ed91b832bc97d6e1
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-08 22:51:38 +0530

    Time: 1 ms (87.79%), Space: 46.5 MB (6.63%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,17 +1,23 @@
 class Solution {
+    public int n;
     public List<List<Integer>> permute(int[] nums) {
+        this.n=nums.length;
         List<List<Integer>> set = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        permut(nums,0,list,set);
+        List<Integer> listleft = new ArrayList<>();
+        for(int x:nums){listleft.add(x);}
+        permut(listleft,0,list,set);
         return set;
     }
-    public void permut(int[] nums,int i,List<Integer> list,List<List<Integer>> set ){
-        if(list.size()==nums.length){set.add(new ArrayList(list));return;}
-        for(int j=0;j<nums.length;j++){
-            if(!list.contains(nums[j])){list.add(nums[j]);}
-            else{continue;}
-            permut(nums,j+1,list,set);
-            list.remove(Integer.valueOf(nums[j]));
+    public void permut(List<Integer> listleft,int i,List<Integer> list,List<List<Integer>> set ){
+        if(list.size()==n){set.add(new ArrayList(list));return;}
+        List<Integer> newlistleft = new ArrayList<>(listleft);
+        for(int j:newlistleft){
+            //if(!list.contains(nums[j])){
+            list.add(j);listleft.remove(Integer.valueOf(j));
+            //}else{continue;}
+            permut(listleft,j+1,list,set);
+            list.remove(Integer.valueOf(j));listleft.add(j);
         }
     }
 }
