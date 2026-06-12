e6bcc3cb399423dedc43d90044b563c8e87365af
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-12 13:42:19 +0530

    Time: 10 ms (58.28%), Space: 47.9 MB (55.71%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int i,n=piles.length,min=1,max=Integer.MIN_VALUE,mid=0,ans=0,k;
+        int i,n=piles.length,min=1,max=Integer.MIN_VALUE,mid=0,ans=0;
         long sum=0;
         for(i=0;i<n;i++){
             sum+=piles[i];
@@ -12,9 +12,8 @@ class Solution {
             sum=0;
             mid=(min+max)/2;
             for(i=0;i<n;i++){
-                if(piles[i]%mid==0){k=piles[i]/mid;}
-                else{k=(piles[i]/mid)+1;}
-                sum+=k;
+                sum+=1+(piles[i]-1)/mid;
+                if(sum>h){break;}
             }
             if(sum<=h){ans=mid;max=mid-1;}
             else{min=mid+1;}
