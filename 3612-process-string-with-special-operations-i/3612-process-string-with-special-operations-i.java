ed95570224f33f2ec64e5a744cefb5a7a1e709f3
Daram Sathvika <daramsathvika23@gmail.com>
2026-06-16 20:19:48 +0530

    Time: 5 ms (20.59%), Space: 55.7 MB (20.59%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){if(sb.length()>0){sb.deleteCharAt(sb.length()-1);}
+            else{continue;}}
+            else if(s.charAt(i)=='#'){
+                StringBuilder sub= new StringBuilder(sb);
+                sb.append(sub);
+            }
+            else if(s.charAt(i)=='%'){sb.reverse();}
+            else{sb.append(s.charAt(i));}
+        }
+        String str = new String(sb);
+        return str;
+    }
+}
