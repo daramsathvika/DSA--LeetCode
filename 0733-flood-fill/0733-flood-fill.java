bf63ed412dc20e61ab26c44da1c228034a48ccd0
Daram Sathvika <daramsathvika23@gmail.com>
2026-07-08 17:46:41 +0530

    Time: 1 ms (38.82%), Space: 46.7 MB (78.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,13 +1,11 @@
 class Solution {
     int m,n,c;
-    boolean [][] vis;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         m=image.length;n=image[0].length;
-        vis= new boolean[m][n];
-        this.vis=vis;
         this.m=m;this.n=n;this.c=image[sr][sc];
+        if(c==color){return image;}
         Queue<int[]> q = new LinkedList<>();
-        image[sr][sc]=color;vis[sr][sc]=true;
+        image[sr][sc]=color;
         q.offer(new int[]{sr,sc});
         int[] arr = new int[2];
         while(!q.isEmpty()){
@@ -15,20 +13,16 @@ class Solution {
             for(int k=0;k<size;k++){
                 arr=q.poll();
                 int i=arr[0],j=arr[1];
-                if(i>0&&image[i-1][j]==c&&!vis[i-1][j]){
-                    vis[i-1][j]=true;
+                if(i>0&&image[i-1][j]==c){
                     image[i-1][j]=color;
                     q.offer(new int[]{i-1,j});}
-                if(i<m-1&&image[i+1][j]==c&&!vis[i+1][j]){
-                    vis[i+1][j]=true;
+                if(i<m-1&&image[i+1][j]==c){
                     image[i+1][j]=color;
                     q.offer(new int[]{i+1,j});} 
-                if(j>0&&image[i][j-1]==c&&!vis[i][j-1]){
-                    vis[i][j-1]=true;
+                if(j>0&&image[i][j-1]==c){
                     image[i][j-1]=color;
                     q.offer(new int[]{i,j-1});}
-                if(j<n-1&&image[i][j+1]==c&&!vis[i][j+1]){
-                    vis[i][j+1]=true;
+                if(j<n-1&&image[i][j+1]==c){
                     image[i][j+1]=color;
                     q.offer(new int[]{i,j+1});} 
             }
