5a954cdd734ea12a1cc83dd3f4cd91c54ce1fba8
Daram Sathvika <daramsathvika23@gmail.com>
2026-07-14 17:39:38 +0530

    Time: 13 ms (7.99%), Space: 60.1 MB (54.35%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,52 @@
+class Solution {
+    int m,n,count;
+    public int numEnclaves(int[][] grid) {
+        this.m=grid.length;n=grid[0].length;
+        int i,j;
+        boolean[][] vis = new boolean[m][n];
+        for(i=0;i<m;i++){
+            if(grid[i][0]==1){dfs(i,0,vis,grid);}
+            vis[i][0]=true;
+            if(grid[i][n-1]==1){dfs(i,n-1,vis,grid);}
+            vis[i][n-1]=true;
+        }
+        for(j=1;j<n-1;j++){
+            if(grid[0][j]==1){dfs(0,j,vis,grid);}
+            vis[0][j]=true;
+            if(grid[m-1][j]==1){dfs(m-1,j,vis,grid);}
+            vis[m-1][j]=true;
+        }
+        for(i=1;i<m-1;i++){
+            for(j=1;j<n-1;j++){
+                if(!vis[i][j]&&grid[i][j]==1){
+                    dfs1(i,j,vis,grid);
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(int i, int j,boolean[][] vis,int[][] grid){
+        if(i<0||i>m-1||j<0||j>n-1||vis[i][j]||grid[i][j]==0){return ;}
+        else{
+            vis[i][j]=true;
+            //System.out.println(i+" "+j);
+            dfs(i-1,j,vis,grid);
+            dfs(i+1,j,vis,grid);
+            dfs(i,j-1,vis,grid);
+            dfs(i,j+1,vis,grid);
+        }
+    }
+    public void dfs1(int i, int j,boolean[][] vis,int[][] grid){
+        if(i<=0||i>=m-1||j<=0||j>=n-1||vis[i][j]||grid[i][j]==0){return;}
+        else{
+            if(grid[i][j]==1){
+                vis[i][j]=true;
+                //System.out.println(i+" "+j);
+                count++;
+            dfs1(i-1,j,vis,grid);
+            dfs1(i+1,j,vis,grid);
+            dfs1(i,j-1,vis,grid);
+            dfs1(i,j+1,vis,grid);}
+        }
+    }
+}
