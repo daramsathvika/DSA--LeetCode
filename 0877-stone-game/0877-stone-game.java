4e1ed7d06c1973ac847d4cc88f3a7f912132fcb0
Daram Sathvika <daramsathvika23@gmail.com>
2026-08-02 22:31:10 +0530

    Time: 0 ms (100%), Space: 42.8 MB (88.59%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // ArrayList<Integer> list = new ArrayList<>();
+
+        // for (int x : piles) {
+        //     list.add(x);
+        // }
+        // int size=list.size(),alice=0,bob=0;
+        // while(size>0){
+        //     if(list.get(0)>list.get(size-1)){
+        //         alice+=list.get(0);
+        //         list.remove(0);}
+        //     else{
+        //         alice+=list.get(size-1);
+        //         list.remove(size-1);
+        //     }--size;
+        //     if(list.get(0)>list.get(size-1)){
+        //         bob+=list.get(0);list.remove(0);}
+        //     else{
+        //         bob+=list.get(size-1);list.remove(size-1);
+        //     }--size;
+        // }
+        // if(alice%2==1){return true;}
+        // else{return false;}
+        return true;
+    }
+}
